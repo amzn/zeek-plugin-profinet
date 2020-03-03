c5313745521003de8e89077085c52486085567d9
Quach <tqquach@amazon.com>
2020-03-02 22:29:51 -0800

    migrating to zeek 3.1.0

--- src/PROFINET.cc
+++ src/PROFINET.cc
@@ -15,7 +15,7 @@ void PROFINET_Analyzer::Done() {
     Analyzer::Done();
     }
 
-void PROFINET_Analyzer::DeliverPacket(int len, const u_char* data, bool orig, uint64 seq, const IP_Hdr* ip, int caplen) {
+void PROFINET_Analyzer::DeliverPacket(int len, const u_char* data, bool orig, uint64_t seq, const IP_Hdr* ip, int caplen) {
     Analyzer::DeliverPacket(len, data, orig, seq, ip, caplen);
 
     try {
